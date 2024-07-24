48b32c4423d127341e288cdb815d6ea88f87f989
Shivam Sharma <115234412+Shivam-a-coder@users.noreply.github.com>
2024-07-25 01:49:26 +0530

    Update Listings.jsx

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -19,8 +19,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://dream-nest-djpm.onrender.com/properties?category=${selectedCategory}`
+          : "https://dream-nest-djpm.onrender.com/properties",
         {
           method: "GET",
         }
